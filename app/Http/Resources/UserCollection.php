63eb5bae719cb476cad643298d1e715467705dbc
sajadk <sajadkaruthedath@gmail.com>
2025-03-25 03:00:58 +0400

    resource and collection

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -14,6 +14,11 @@ class UserCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => UserResource::collection($this->collection),
+            'meta' => [
+                'total_users' => $this->collection->count(),
+            ],
+        ];
     }
 }
